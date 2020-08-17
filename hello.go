1bed49e95a9a5f8fac66c5d64e0a0d2880d19c53
Levi <27644153+levitannin@users.noreply.github.com>
2020-08-17 10:58:30 -0400

    Update hello.go

--- hello.go
+++ hello.go
@@ -1,3 +1,7 @@
+//	First Go program created to test the installation process.
+//	Followed the following directions:
+//		https://www.digitalocean.com/community/tutorials/how-to-install-go-on-debian-10
+
 package main
 import "fmt"
 
