0d5230094025b9dc56a52799e3cd07e8fa6557d6
Levi <27644153+levitannin@users.noreply.github.com>
2020-09-05 19:05:44 -0400

    Add files via upload

--- hello.go
+++ hello.go
@@ -1,10 +1,14 @@
-//	First Go program created to test the installation process.
-//	Followed the following directions:
-//		https://www.digitalocean.com/community/tutorials/how-to-install-go-on-debian-10
+//	Small program for installing a package (quote)
+//	When run, will downlaod package (if needed)
+//		and will print out the following quote:
+//	"Don't communicate by sharing memory,
+//		share memory by communicating."
 
 package main
-import "fmt"
 
-func main(){
-	fmt.Printf("Hello, World!\n")
+import "fmt"
+import "rsc.io/quote"
+
+func main() {
+	fmt.Println(quote.Go())
 }
