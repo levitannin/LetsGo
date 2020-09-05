f83395c1abaf5b2b187883c6e44c8b8b1f035709
Levi <27644153+levitannin@users.noreply.github.com>
2020-09-05 19:10:24 -0400

    Update hello.go

--- hello/hello.go
+++ hello/hello.go
@@ -1,23 +1,13 @@
 //	First Go program created to test the installation process.
-//	Small program for installing a package (quote)
 //	Followed the following directions:
-//	When run, will downlaod package (if needed)
 //		https://www.digitalocean.com/community/tutorials/how-to-install-go-on-debian-10
-//		and will print out the following quote:
-//	"Don't communicate by sharing memory,
-//		share memory by communicating."
 
 
 package main
-package main
 
-import "fmt"
 import "fmt"
 import "rsc.io/quote"
 
-
-func main(){
 func main() {
 	fmt.Printf("Hello, World!\n")
-	fmt.Println(quote.Go())
 }
